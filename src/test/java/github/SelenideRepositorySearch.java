e8ff356b670d83498ae2ab14d02ed795560df988
qawafsec <mkd@weblock.su>
2024-05-05 01:13:35 +0300

    changed test logic

--- src/test/java/github/SelenideRepositorySearch.java
+++ src/test/java/github/SelenideRepositorySearch.java
@@ -1,28 +1,41 @@
 package github;
 
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Configuration.*;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 
 public class SelenideRepositorySearch {
+  @BeforeAll
+  static void configuration() {
+    String pageLoadStrategy = "eager";
+  }
 
   @Test
   void shouldFindSelenideRepositoryAtTheTop() {
 
-    // открыть главную страницу
-    open("https://github.com/");
-    //open("https://github.com/");
-    // ввести в поле поиска selenide и нажать enter
-    $("[placeholder='Search or jump to...']").click();
-    //$("[placeholder='Search GitHub']").setValue("selenide").pressEnter();
-    // кликнуть на первый репозиторий из списка найденых
-    //$$("ul.repo-list li").first().$("a").click();
-    // проверка: заголовок selenide/selenide
-    //$("#repository-container-header").shouldHave(text("selenide / selenide"));
+    open("https://github.com/selenide/selenide");
+    $("#wiki-tab").click();
+    $("#wiki-body").shouldHave(text("Soft assertions"));
+    $("#wiki-body").$(byText("Soft assertions")).click();
+    $("#wiki-content").shouldHave(text("Using JUnit5 extend test class:"));
+    $("#wiki-content").shouldHave(text("@ExtendWith({SoftAssertsExtension.class})\n" +
+            "class Tests {\n" +
+            "@Test\n" +
+            "void test() {\n" +
+            "Configuration.assertionMode = SOFT;\n" +
+            "open(\"page.html\");\n" +
+            "$(\"#first\").should(visible).click();\n" +
+            "$(\"#second\").should(visible).click();\n" +
+            "}\n" +
+            "}"));
+
 sleep(5000);
 
     // ARRANGE
